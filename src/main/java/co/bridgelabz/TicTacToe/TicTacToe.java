9c59386e09a39c403a762985381eec5517b30828
iamsr5675 <saurabhranjan5675@gmail.com>
2021-09-10 03:26:16 +0530

    toss to check who play first

--- src/main/java/co/bridgelabz/TicTacToe/TicTacToe.java
+++ src/main/java/co/bridgelabz/TicTacToe/TicTacToe.java
@@ -7,6 +7,7 @@ public class TicTacToe {
 	static char[] board = new char[10];
 	static char player,computer;
 	private static int playLocation;
+	private static int tossResult;
 	
 	public static void createBoard(){
 		for(int index = 1 ; index<10 ; index++) {
@@ -16,7 +17,7 @@ public class TicTacToe {
 	
 	public static void getPlayerChoice() {
 
-		 System.out.print("select X or O : ");
+		 System.out.print("Select X or O : ");
 		 player = Character.toUpperCase(scannerObject.next().charAt(0));
 		 
 		 if (player == 'X')
@@ -27,32 +28,61 @@ public class TicTacToe {
 		 System.out.println("Computer's choice is : " +computer);
 	}
 	
+	
 	public static void showBoard() {
+		System.out.println();
         System.out.println("  " + board[1] + "  |  " + board[2]  + "   | " + board[3] + "  ");
+        System.out.println("------------------");
         System.out.println("  " + board[4] + "  |  " + board[5]  + "   | " + board[6] + "  ");
+        System.out.println("------------------");
         System.out.println("  " + board[7] + "  |  " + board[8]  + "   | " + board[9] + "  ");
 	}
 	
+
 	public static void userMove() {
-		System.out.println("Enter Location 1-9 to Make Move");
+		
+		showBoard();
+		System.out.println("\nEnter Location 1-9 to Make Move");
 		playLocation = scannerObject.nextInt();
-		if (playLocation < 10 && playLocation > 0) {
+
+		scannerObject.nextLine();
+		if (isEmpty() && playLocation < 10 && playLocation > 0) {
+
 			board[playLocation] = player;
 			showBoard();
 		} else {
 			System.out.println("Invalid Choice");
+
 		}
 	}
 	
-	public static void main(String[] args) {
-		
-		System.out.println("----- Welcome To The Game Of Tic Tac Toe -----");
-		createBoard();
-		getPlayerChoice();
-		showBoard();
-		userMove();
-		
+	public static boolean isEmpty() {
+		if (board[playLocation] == ' ') {
+			return true;
+		} else {
+			return false;
+		}
 
 	}
-
+	
+	public static void checkToss() {
+		
+		double tossResult = Math.floor(Math.random() * 10) % 2;
+		System.out.println("\nChoose 1 for Heads or 2 for Tails");
+		int coinSelect = scannerObject.nextInt();
+		if (coinSelect == tossResult) {
+			System.out.println("\nPlayer Won The Toss! Player Starts");
+		} else {
+			System.out.println("\nComputer Won The Toss! Computer Starts");
+		}
+	}
+		
+	public static void main(String[] args) {
+		
+		System.out.println("----- Welcome To The Game Of Tic Tac Toe -----\n");
+		createBoard();
+		getPlayerChoice();
+		checkToss();
+		userMove();
+	}
 }
