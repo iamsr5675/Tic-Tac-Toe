fdfded3aa2d1026a9f296a05c82a03f3fde8f204
iamsr5675 <saurabhranjan5675@gmail.com>
2021-09-10 03:06:57 +0530

    User's desired move

--- src/main/java/co/bridgelabz/TicTacToe/TicTacToe.java
+++ src/main/java/co/bridgelabz/TicTacToe/TicTacToe.java
@@ -3,19 +3,21 @@ package co.bridgelabz.TicTacToe;
 import java.util.Scanner;
 
 public class TicTacToe {
+	public static Scanner scannerObject = new Scanner(System.in);
 	static char[] board = new char[10];
 	static char player,computer;
-	public static Scanner scanner = new Scanner(System.in);
+	private static int playLocation;
 	
-	static void createBoard(){
-		for(int index = 1 ; index < 10 ; index++) {
+	public static void createBoard(){
+		for(int index = 1 ; index<10 ; index++) {
 			board[index] = ' ';
 		}
 	}
 	
-	static void getPlayerChoice() {
+	public static void getPlayerChoice() {
+
 		 System.out.print("Select X or O : ");
-		 player = Character.toUpperCase(scanner.next().charAt(0));
+		 player = Character.toUpperCase(scannerObject.next().charAt(0));
 		 
 		 if (player == 'X')
 			 computer='O';
@@ -25,16 +27,43 @@ public class TicTacToe {
 		 System.out.println("Computer's choice is : " +computer);
 	}
 	
-	static void showBoard() {
+	
+	public static void showBoard() {
+		System.out.println();
         System.out.println("  " + board[1] + "  |  " + board[2]  + "   | " + board[3] + "  ");
+        System.out.println("------------------");
         System.out.println("  " + board[4] + "  |  " + board[5]  + "   | " + board[6] + "  ");
+        System.out.println("------------------");
         System.out.println("  " + board[7] + "  |  " + board[8]  + "   | " + board[9] + "  ");
 	}
 	
+
+	public static void userMove() {
+		System.out.println("Enter Location 1-9 to Make Move");
+		playLocation = scannerObject.nextInt();
+		scannerObject.nextLine();
+		if (isEmpty() && playLocation < 10 && playLocation > 0) {
+			board[playLocation] = player;
+			showBoard();
+		} else {
+			System.out.println("Invalid Choice");
+		}
+	}
+	
+	public static boolean isEmpty() {
+		if (board[playLocation] == ' ') {
+			return true;
+		} else {
+			return false;
+		}
+	}
+		
 	public static void main(String[] args) {
 		
-		System.out.println("----- Welcome To The Game Of Tic Tac Toe -----");
+		System.out.println("----- Welcome To The Game Of Tic Tac Toe -----\n");
 		createBoard();
 		getPlayerChoice();
+		showBoard();
+		userMove();
 	}
 }
